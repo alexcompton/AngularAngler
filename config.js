f986b2454ca55a83f0e67353e0743d2fddaebca0
alexcompton <alexcompton11@gmail.com>
2016-02-28 16:33:23 -0500

    added pages

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://alexcompton11:A1s2d3f4!@ds019068.mlab.com:19068/angular-angler',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
